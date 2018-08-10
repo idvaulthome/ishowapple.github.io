5fa4594d61837910e1f698ee14097e154d8903ab
Ishowapple <40322263+Ishowapple@users.noreply.github.com>
2018-08-09 22:31:37 -0400

    Delete submit.php

--- complete/submit.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$a = $_POST['udid'] . "\n" . $_POST['name'] . "\n" . $_POST['email'] . "\n" . $_POST['product'];
-	mail("YOU@YOUR-DOMAIN.c-o-m", "UDID", $a);
-	header('Location: finished.html');
-?>
-	
